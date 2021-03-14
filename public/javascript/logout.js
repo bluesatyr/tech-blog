cc32f7e0ebd0a805a97a7a546d72809f7e5325b9
bluesatyr <shawnevans.music@gmail.com>
2021-03-14 15:51:37 -0400

    add auto logout after 5 minutes idle

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,3 +1,6 @@
+var timeoutInMiliseconds = 300000; // five minutes of idleness before auto logout
+var timeoutId; 
+
 async function logout() {
     const response = await fetch('/api/users/logout', {
         method: 'post',
@@ -11,4 +14,23 @@ async function logout() {
     }
 }
 
+function startTimer() { 
+    // window.setTimeout returns an Id that can be used to start and stop a timer
+    timeoutId = window.setTimeout(logout, timeoutInMiliseconds)
+}
+
+function resetTimer() { 
+    window.clearTimeout(timeoutId)
+    startTimer();
+}
+ 
+function setupTimers () {
+    document.addEventListener("mousemove", resetTimer, false);
+    document.addEventListener("mousedown", resetTimer, false);
+    document.addEventListener("keypress", resetTimer, false);
+    document.addEventListener("touchmove", resetTimer, false);
+    startTimer();
+}
+
+window.addEventListener('load', startTimer);
 document.querySelector('#logout').addEventListener('click', logout);
